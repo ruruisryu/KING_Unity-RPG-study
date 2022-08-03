58d714249e071d09f128d5d0d1cb6aea53e39ade
Seohyun Ryu <79790536+Ryuryu128@users.noreply.github.com>
2022-08-04 00:33:13 +0900

    Update UI_Base.cs

--- Unity/UI/UI_Base.cs
+++ Unity/UI/UI_Base.cs
@@ -1,47 +1,107 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class UI_Base : MonoBehaviour
+public class UIManager 
 {
-    Dictionary<Type, UnityEngine.Object[]> _objects = new Dictionary<Type, UnityEngine.Object[]>();
+    // 팝업이 하나도 띄워지지 않은 상태에서는 _order값이 0이다
+    // 이때 생성된 팝업의 Sort Order값은 0인데, 이 값은 Sort하지 않는 UI와 같은 값이다
+    // _order를 10부터 시작하도록 바꾸고 0부터 9까지는 혹시 더 먼저 띄우고 싶은 창이 있다면 그 창에게 값을 내주도록 한다
+    int _order = 10;
 
-    protected void Bind<T>(Type type) where T : UnityEngine.Object
+    Stack<UI_Popup> _popupStack = new Stack<UI_Popup>();
+    UI_Scene _sceneUI = new UI_Scene();
+
+    // 프로퍼티로 생성
+    public GameObject Root
     {
-        string[] names = Enum.GetNames(type);
-
-        UnityEngine.Object[] objects = new UnityEngine.Object[names.Length];
-        _objects.Add(typeof(T), objects);
-
-        for (int i = 0; i < names.Length; i++)
+        get
         {
-            if (typeof(T) == typeof(GameObject))
-                objects[i] = Util.FindChild(gameObject, names[i], true);
-            else
-                objects[i] = Util.FindChild<T>(gameObject, names[i], true);
+            GameObject root = GameObject.Find("@UI_Root");
+            if (root == null)
+                root = new GameObject { name = "@UI_Root" };
+
+            return root;
         }
     }
 
-    protected T Get<T>(int index) where T : UnityEngine.Object
+    public void SetCanvas(GameObject go, bool sort = true)
     {
-        UnityEngine.Object[] objects = null;
-        if (_objects.TryGetValue(typeof(T), out objects) == false)
-            return null;
+        Canvas canvas = Util.GetOrAddComponent<Canvas>(go);
+        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        canvas.overrideSorting = true;
 
-        // T로 캐스팅
-        return objects[index] as T;
+        if (sort)
+        {
+            canvas.sortingOrder = (_order);
+            _order++;
+        }
+        else
+        {
+            canvas.sortingOrder = 0;
+        }
     }
 
-    int _score = 0;
-
-    public void OnButtonClicked()
+    public T ShowSceneUI<T>(string name = null) where T : UI_Scene
     {
-        _score++;
+        if (string.IsNullOrEmpty(name))
+            name = typeof(T).Name;
+
+        GameObject go = Managers.Resource.Instantiate($"UI/Scene/{name}");
+
+        T sceneUI = Util.GetOrAddComponent<T>(go);
+        _sceneUI = sceneUI;
+
+        go.transform.SetParent(Root.transform);
+
+        return sceneUI;
     }
 
-    protected Text GetText(int index) { return Get<Text>(index); }
-    protected Button GetButton(int index) { return Get<Button>(index); }
-    protected Image GetImage(int index) { return Get<Image>(index); }
+    public T ShowPopupUI<T>(string name = null) where T: UI_Popup 
+    {
+        if (string.IsNullOrEmpty(name))
+            name = typeof(T).Name;
+
+        GameObject go = Managers.Resource.Instantiate($"UI/Popup/{name}");
+
+        T popup = Util.GetOrAddComponent<T>(go);
+        _popupStack.Push(popup);
+
+        go.transform.SetParent(Root.transform); 
+
+            return popup;
+    }
+
+    public void ClosePopupUI()
+    {
+        if (_popupStack.Count == 0)
+            return;
+
+        UI_Popup popup = _popupStack.Pop();
+        Managers.Resource.Destroy(popup.gameObject);
+        popup = null;
+
+        _order--;
+    }
+
+    public void ClosePopupUI(UI_Popup popup)
+    {
+        if (_popupStack.Count == 0)
+            return;
+
+        if (_popupStack.Peek() != popup)
+        {
+            Debug.Log("Close Popup Failed!");
+            return;
+        }
+
+        ClosePopupUI();
+    }
+
+    // 모든 팝업 닫기
+    public void CloseAllPopupUI()
+    {
+        while (_popupStack.Count > 0)
+            ClosePopupUI();
+    }
 }
